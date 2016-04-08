7be82c380d9036cadc41a5bb2055b4ac9f9ce5ac
Eleonore9 <eleonore.mayola@ymail.com>
2016-04-08 13:43:13 +0100

    Set up new clj project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to witan.models.demography
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/witan/models/demography_test.clj
@@ -0,0 +1,7 @@
+(ns witan.models.demography-test
+  (:require [clojure.test :refer :all]
+            [witan.models.demography :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
