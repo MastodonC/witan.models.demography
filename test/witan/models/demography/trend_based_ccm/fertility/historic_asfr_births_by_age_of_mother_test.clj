959077e62bb0d0eb112fe586592effa6a2651ed2
Lora Armstrong <lora.s.armstrong@gmail.com>
2016-04-13 14:53:20 +0100

    Add historic ASFR by age of mother files

---
+++ test/witan/models/demography/trend_based_ccm/fertility/historic_asfr_births_by_age_of_mother_test.clj
@@ -0,0 +1,3 @@
+(ns witan.models.demography.trend-based-ccm.fertility.historic-asfr-births-by-age-of-mother-test
+  (:require [clojure.test :refer :all]
+            [witan.models.demography.trend-based-ccm.fertility.historic-asfr-births-by-age-of-mother :refer :all]))
--- test/witan/models/demography_test.clj
+++
@@ -1,7 +0,0 @@
-(ns witan.models.demography-test
-  (:require [clojure.test :refer :all]
-            [witan.models.demography :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
