eeeb586d888178b05e50ae7f70057a49f4c9c608
Lora Armstrong <lora.s.armstrong@gmail.com>
2016-04-13 15:17:34 +0100

    Change from @otfrom's comment

--- src/witan/models/demography/trend_based_ccm/fertility/historic_asfr_births_by_age_of_mother.clj
+++ src/witan/models/demography/trend_based_ccm/fertility/historic_asfr_births_by_age_of_mother.clj
@@ -1,6 +1,1 @@
 (ns witan.models.demography.trend-based-ccm.fertility.historic-asfr-births-by-age-of-mother)
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "When I'm finished I will calculate historic age-specific fertility rates, using births by age of mother as an input!"))
