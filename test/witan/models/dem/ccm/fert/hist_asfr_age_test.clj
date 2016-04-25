4b13f2bce5fbce98a636cb94d7122fdeef2cbe4e
Lora Armstrong <lora.s.armstrong@gmail.com>
2016-04-25 10:53:14 +0100

    Rename gss code key, fix a few small errors
    Rename gss code key in model functions
    
    Rename gss code key in tests
    
    Fix joining error in ->births-pool function
    
    Correct error in test

--- test/witan/models/dem/ccm/fert/hist_asfr_age_test.clj
+++ test/witan/models/dem/ccm/fert/hist_asfr_age_test.clj
@@ -152,53 +152,53 @@
 ;; End of input data handling
 
 ;; Functions take maps of all inputs/outputs from parent nodes in workflow
-;; (def from-births-data-year (merge data-inputs
-;;                                   (->births-data-year data-inputs)))
-;; (def for-births-pool (merge from-births-data-year
-;;                             (->at-risk-this-year from-births-data-year)
-;;                             (->at-risk-last-year from-births-data-year)))
+(def from-births-data-year (merge data-inputs
+                                  (->births-data-year data-inputs)))
+(def for-births-pool (merge from-births-data-year
+                            (->at-risk-this-year from-births-data-year)
+                            (->at-risk-last-year from-births-data-year)))
 
-;; (def from-births-pool (merge for-births-pool
-;;                              (->births-pool for-births-pool)))
+(def from-births-pool (merge for-births-pool
+                             (->births-pool for-births-pool)))
 ;; End of data map creation
 
 (defn- same-coll? [coll1 coll2]
   (= (set coll1) (set coll2)))
 
 ;; Tests
-;; (deftest ->births-data-year-test
-;;   (testing "The latest year is returned"
-;;     (is (= 2013
-;;            (:yr (->births-data-year data-inputs))))))
+(deftest ->births-data-year-test
+  (testing "The latest year is returned"
+    (is (= 2013
+           (:yr (->births-data-year data-inputs))))))
 
-;; (deftest ->at-risk-this-year-test
-;;   (testing "The data transformation returns the correct columns"
-;;     (is (same-coll? [:gss.code :sex :popn-this-yr :age]
-;;                     (ds/column-names (:at-risk-this-year
-;;                                       (->at-risk-this-year from-births-data-year))))))
-;;   (testing "The age column range is now 0-89 instead of 1-90"
-;;     (let [former-age-range (distinct (i/$ :age (:at-risk-popn data-inputs)))
-;;           min-former-range (reduce min former-age-range)
-;;           max-former-range (reduce max former-age-range)]
-;;       (is (same-coll? (range (dec min-former-range) max-former-range)
-;;                       (distinct (i/$ :age (:at-risk-this-year
-;;                                            (->at-risk-this-year from-births-data-year)))))))))
+(deftest ->at-risk-this-year-test
+  (testing "The data transformation returns the correct columns"
+    (is (same-coll? [:gss-code :sex :popn-this-yr :age]
+                    (ds/column-names (:at-risk-this-year
+                                      (->at-risk-this-year from-births-data-year))))))
+  (testing "The age column range is now 0-89 instead of 1-90"
+    (let [former-age-range (distinct (i/$ :age (:at-risk-popn data-inputs)))
+          min-former-range (reduce min former-age-range)
+          max-former-range (reduce max former-age-range)]
+      (is (same-coll? (range (dec min-former-range) max-former-range)
+                      (distinct (i/$ :age (:at-risk-this-year
+                                           (->at-risk-this-year from-births-data-year)))))))))
 
-;; (deftest ->at-risk-last-year-test
-;;   (testing "The data is filtered by the correct year"
-;;     (is (same-coll? '(2013)
-;;                     (distinct (i/$ :year (:at-risk-last-year
-;;                                           (->at-risk-last-year from-births-data-year)))))))
-;;   (testing "The data transformation returns the correct columns"
-;;     (is (same-coll? [:gss.code :sex :age :year :popn-last-yr]
-;;                     (ds/column-names (:at-risk-last-year
-;;                                       (->at-risk-last-year from-births-data-year)))))))
+(deftest ->at-risk-last-year-test
+  (testing "The data is filtered by the correct year"
+    (is (same-coll? '(2013)
+                    (distinct (i/$ :year (:at-risk-last-year
+                                          (->at-risk-last-year from-births-data-year)))))))
+  (testing "The data transformation returns the correct columns"
+    (is (same-coll? [:gss-code :sex :age :year :popn-last-yr]
+                    (ds/column-names (:at-risk-last-year
+                                      (->at-risk-last-year from-births-data-year)))))))
 
-;; (deftest ->births-pool-test
-;;   (testing "The data transformation returns the correct columns"
-;;     (is (same-coll? [:age :sex :gss.code :year :birth-pool]
-;;                     (ds/column-names (:births-pool (->births-pool for-births-pool))))))
-;;   (testing "No nil or NaN in the birth-pool column"
-;;     (is (not-any? nil? (i/$ :birth-pool (:births-pool (->births-pool for-births-pool)))))
-;;     (is (not-any? #(Double/isNaN %)
-;;                   (i/$ :birth-pool (:births-pool (->births-pool for-births-pool)))))))
+(deftest ->births-pool-test
+  (testing "The data transformation returns the correct columns"
+    (is (same-coll? [:age :sex :gss-code :year :birth-pool]
+                    (ds/column-names (:births-pool (->births-pool for-births-pool))))))
+  (testing "No nil or NaN in the birth-pool column"
+    (is (not-any? nil? (i/$ :birth-pool (:births-pool (->births-pool for-births-pool)))))
+    (is (not-any? #(Double/isNaN %)
+                  (i/$ :birth-pool (:births-pool (->births-pool for-births-pool)))))))
