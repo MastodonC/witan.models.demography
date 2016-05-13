f60bb01143a5a86f0d61690b07755a0db17fcf62
Lora Armstrong <lora.s.armstrong@gmail.com>
2016-05-13 17:09:38 +0100

    Add fn to age-on population; tests for age-on
    Update project.clj to fix dependency issues
    
    Rm unused fn, test values, compare to R.
    
    Rm incanter-io, add test for `age-on`.
    
    Finish age-on tests (1 failure as age-on not done)
    
    Use witan.workspace-api rollup fn to finish age-on

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [prismatic/schema "1.0.4"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [prismatic/schema "1.1.0"]
                  [clojure-csv/clojure-csv "2.0.1"]
                  [net.mikera/core.matrix "0.51.0"]
                  [witan.workspace-executor "0.1.3"]
                  [kixi/incanter-core "1.9.1-p0-3bf644a"]
                  [witan.workspace-api "0.1.0-SNAPSHOT"]]
-  :main ^:skip-aot witan.models.demography
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :exclusions [prismatic/schema org.clojure/clojure])
--- test/witan/models/dem/ccm/core/projection_loop_test.clj
+++ test/witan/models/dem/ccm/core/projection_loop_test.clj
@@ -15,7 +15,7 @@
                    "resources/test_data/bristol_hist_popn_est.csv"}))
 
 (def params {:first-proj-year 2014
-             :last-proj-year 2017})
+             :last-proj-year 2015})
 
 ;; Useful fn:
 (defn- same-coll? [coll1 coll2]
@@ -27,4 +27,26 @@
     (let [get-start-popn (select-starting-popn data-inputs)]
       (is (same-coll? [:gss-code :sex :age :year :popn]
                       (ds/column-names (:population get-start-popn))))
-      (is (= 2015 (get-last-yr-from-popn (:population get-start-popn)))))))
+      (is (= 2015 (get-last-yr-from-popn (:population get-start-popn))))
+      ;; Total number of 15 yrs old in 2015:
+      (is (= 4386 (apply + (i/$ :popn
+                                (i/query-dataset (:population get-start-popn)
+                                                 {:year 2015 :age 15})))))
+      ;; Total number of women aged between 40 and 43 in 2015:
+      (is (= 5394 (apply + (i/$ :popn
+                                (i/query-dataset (:population get-start-popn)
+                                                 {:sex "F" :age {:$gt 40 :lt 43}
+                                                  :year 2015}))))))))
+
+(deftest age-on-test
+  (testing "Popn is aged on correctly, 90 grouping is handled correctly."
+    (let [starting-popn (:population (select-starting-popn data-inputs))
+          aged-popn (:population (age-on (select-starting-popn data-inputs)))
+          sum-popn (fn [popn year age]
+                     (apply + (i/$ :popn (i/query-dataset popn
+                                                          {:year year :age age}))))]
+      (is (= (sum-popn starting-popn 2015 15)
+             (sum-popn aged-popn 2015 16)))
+      (is (= (+ (sum-popn starting-popn 2015 89) (sum-popn starting-popn 2015 90))
+             (sum-popn aged-popn 2015 90)))
+      (is (= 2 (first (:shape (i/query-dataset aged-popn {:year 2015 :age 90}))))))))
