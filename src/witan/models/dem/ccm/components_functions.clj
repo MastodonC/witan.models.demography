6a32fb31f652baf19c458f19c26cf7442c992357
Eleonore9 <eleonore.mayola@ymail.com>
2016-05-25 11:48:01 +0100

    Finish the average function and test.
    Fix typo, fix naming.

--- src/witan/models/dem/ccm/components_functions.clj
+++ src/witan/models/dem/ccm/components_functions.clj
@@ -1,16 +1,17 @@
 (ns witan.models.dem.ccm.components-functions
   (:require [incanter.core :as i]
-            [clojure.core.matrix.dataset :as ds]))
+            [clojure.core.matrix.dataset :as ds]
+            [witan.datasets :as wds]))
 
 ;; Calculate rates/values for alternative ways to project components of change:
 ;; 1) Calculate averages for fixed values/rates or trends:
 (defn calculate-averages
   "Takes in a dataset with historical data, a number of years to average on
   and the jumpoff year. Returns a datasets with a new column containing the
-  averages for a specific component for all the years of interest."
-  [component historical-data nb-of-years jumpoff-year]
+  averages for the estimate column for all the years of interest."
+  [historical-data nb-of-years jumpoff-year]
   (let [last-yr-data (dec jumpoff-year)
-        start-year (- last-yr-data nb-of-years)
+        start-year (inc (- last-yr-data nb-of-years))
         data-of-interest (i/query-dataset historical-data
                                           {:year {:$gte start-year :$lte last-yr-data}})]
-    ))
+    (wds/rollup :mean :estimate [:gss-code :sex :age :var] data-of-interest)))
--- test/witan/models/dem/ccm/components_functions_test.clj
+++ test/witan/models/dem/ccm/components_functions_test.clj
@@ -5,6 +5,7 @@
             [incanter.core :as i]
             [witan.models.load-data :as ld]))
 
+(defn- fp-equals? [x y ε] (< (Math/abs (- x y)) ε))
 
 (def domestic-mig-in (ld/load-dataset :migration-estimates
                                       "resources/test_data/bristol_dom_in_mig.csv"))
@@ -13,5 +14,11 @@
                                       "resources/test_data/bristol_dom_in_avg.csv"))
 
 (deftest calculate-averages-test
-  (testing "The fonction return the averages on the right year period"
-    ))
+  (testing "The function return the averages on the right year period"
+    (let [r-results (:dom-in-averages dom-in-averages)
+          clj-results (calculate-averages (:migration-estimates domestic-mig-in) 12 2015)
+          joined-averages (i/$join [[:gss-code :sex :age] [:gss-code :sex :age]]
+                                 r-results clj-results)]
+      (is (every? #(fp-equals? (i/sel joined-averages :rows % :cols :domin)
+                               (i/sel joined-averages :rows % :cols :estimate) 0.0001)
+                  (range (first (:shape joined-averages))))))))
