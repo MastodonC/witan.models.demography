de8474ec70e1a7e6239b6b122de6a6dd7e86f941
Lora Armstrong <lora.s.armstrong@gmail.com>
2016-05-26 16:37:23 +0100

    Test all four migration projection

--- test/witan/models/dem/ccm/components_functions_test.clj
+++ test/witan/models/dem/ccm/components_functions_test.clj
@@ -7,8 +7,14 @@
 
 (defn- fp-equals? [x y ε] (< (Math/abs (- x y)) ε))
 
-(def domestic-mig-in (ld/load-dataset :migration-mye
-                                      "resources/test_data/migration/bristol_dom_in_mig.csv"))
+(def migration-data (ld/load-datasets {:domestic-in-migrants
+                                       "resources/test_data/migration/bristol_dom_in_mig.csv"
+                                        :domestic-out-migrants
+                                       "resources/test_data/migration/bristol_dom_out_mig.csv"
+                                        :international-in-migrants
+                                       "resources/test_data/migration/bristol_inter_in_mig.csv"
+                                        :international-out-migrants
+                                       "resources/test_data/migration/bristol_inter_in_mig.csv"}))
 
 (def dom-in-averages (ld/load-dataset :dom-in-averages
                                       "resources/test_data/migration/bristol_dom_in_avg.csv"))
@@ -25,10 +31,35 @@
 (deftest calculate-averages-test
   (testing "The function return the averages on the right year period"
     (let [r-results (:dom-in-averages dom-in-averages)
-          clj-results (calculate-averages (:migration-mye domestic-mig-in)
+          clj-results (calculate-averages (:domestic-in-migrants migration-data)
                                           :estimate :domestic-in 12 2015)
           joined-averages (i/$join [[:gss-code :sex :age] [:gss-code :sex :age]]
                                    r-results clj-results)]
       (is (every? #(fp-equals? (i/sel joined-averages :rows % :cols :domin)
                                (i/sel joined-averages :rows % :cols :domestic-in) 0.0001)
+                  (range (first (:shape joined-averages))))))
+    (let [r-results (:dom-out-averages dom-out-averages)
+          clj-results (calculate-averages (:domestic-out-migrants migration-data)
+                                          :estimate :domestic-out 12 2015)
+          joined-averages (i/$join [[:gss-code :sex :age] [:gss-code :sex :age]]
+                                   r-results clj-results)]
+      (is (every? #(fp-equals? (i/sel joined-averages :rows % :cols :domout)
+                               (i/sel joined-averages :rows % :cols :domestic-out) 0.0001)
+                  (range (first (:shape joined-averages))))))
+    (let [r-results (:inter-out-averages inter-out-averages)
+          clj-results (calculate-averages (:international-out-migrants migration-data)
+                                          :estimate :international-out 12 2015)
+          joined-averages (i/$join [[:gss-code :sex :age] [:gss-code :sex :age]]
+                                   r-results clj-results)]
+      (is (every? #(fp-equals? (i/sel joined-averages :rows % :cols :intout)
+                               (i/sel joined-averages :rows % :cols :international-out) 0.0001)
+                  (range (first (:shape joined-averages))))))
+    (let [r-results (:inter-in-averages inter-in-averages)
+          clj-results (calculate-averages (:international-in-migrants migration-data)
+                                          :estimate :international-in 12 2015)
+          joined-averages (i/$join [[:gss-code :sex :age] [:gss-code :sex :age]]
+                                   r-results clj-results)
+          _ (println joined-averages)]
+      (is (every? #(fp-equals? (i/sel joined-averages :rows % :cols :intin)
+                               (i/sel joined-averages :rows % :cols :international-in) 0.0001)
                   (range (first (:shape joined-averages))))))))
