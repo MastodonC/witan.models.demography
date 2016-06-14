47c3421526756f9641a7aab1fdd4305a9b1b178c
Lora Armstrong <lora.s.armstrong@gmail.com>
2016-06-14 14:18:32 +0100

    Project ASFR, Combine by sex fns + tests
    Combine births by sex + test

---
+++ src/witan/models/dem/ccm/fert/fertility_mvp.clj
@@ -0,0 +1,51 @@
+(ns witan.models.dem.ccm.fert.fertility-mvp
+  (:require [witan.models.dem.ccm.components-functions :as cf]
+            [witan.workspace-api :refer [defworkflowfn]]
+            [clojure.core.matrix.dataset :as ds]
+            [witan.datasets :as wds]
+            [witan.models.dem.ccm.schemas :refer :all]
+            [schema.core :as s]))
+
+(defworkflowfn project-asfr-finalyrhist-fixed
+  "Takes dataset of historic age specific fertility rates, and parameter
+   for the last year of fertility data. Returns dataset with projected
+   age specific fertility rates, calculated using the jumpoff year average
+   method (see docs)."
+  {:witan/name :ccm-fert/project-asfr-finalyrhist-fixed
+   :witan/version "1.0"
+   :witan/input-schema {:historic-asfr HistASFRSchema}
+   :witan/param-schema {:fert-last-yr s/Int}
+   :witan/output-schema {:initial-projected-fertility-rates ProjFixedASFRSchema}}
+  [{:keys [historic-asfr]} {:keys [fert-last-yr]}]
+  {:initial-projected-fertility-rates (cf/jumpoffyr-method-average historic-asfr
+                                                                   :fert-rate
+                                                                   :fert-rate
+                                                                   1
+                                                                   (inc fert-last-yr))})
+
+(defn- gather-births-by-sex
+  "Given a dataset with columns :gss-code, :m, and :f (where :m and :f are
+   male and female births), gathers births data into :births column and 
+   sex into :sex column, returning a new dataset. Standin for a universal 
+   gather function in witan.datasets similar to gather in tidyR"
+  [births-by-mf]
+  (let [births-f (-> births-by-mf
+                     (ds/add-column :sex (repeat "F"))
+                     (ds/rename-columns {:f :births})
+                     (ds/select-columns [:gss-code :sex :births]))
+        births-m (-> births-by-mf
+                     (ds/add-column :sex (repeat "M"))
+                     (ds/rename-columns {:m :births})
+                     (ds/select-columns [:gss-code :sex :births]))]
+    (ds/join-rows births-m births-f)))
+
+(defn combine-into-births-by-sex
+  "Takes a dataset with births by age and sex of mother, and pm, the fraction
+   of births that are male (from 0-1)"
+  [births-by-agesex pm]
+  (-> births-by-agesex
+      (wds/rollup :sum :births [:gss-code])
+      (wds/add-derived-column :m [:births] (fn [b] (* pm b)))
+      (wds/add-derived-column :f [:births] (fn [b] (* (- 1 pm) b)))
+      (ds/select-columns [:gss-code :m :f])
+      gather-births-by-sex))
---
+++ test/witan/models/dem/ccm/fert/fertility_mvp_test.clj
@@ -0,0 +1,67 @@
+(ns witan.models.dem.ccm.fert.fertility-mvp-test
+  (:require [witan.models.dem.ccm.fert.fertility-mvp :refer :all]
+            [witan.models.load-data :as ld]
+            [clojure.test :refer :all]
+            [incanter.core :as i]
+            [clojure.core.matrix.dataset :as ds]
+            [witan.datasets :as wds]))
+
+(defn- fp-equals? [x y ε] (< (Math/abs (- x y)) ε))
+
+;;;;;;;;;;;;;;;;;
+;; Test inputs ;;
+;;;;;;;;;;;;;;;;;
+
+;; historic-asmr = output of calc historic ASMR fn from R, to use until
+;;                 a Clojure version exists
+
+;;proj-births-age-sex-mother = output of project.births.from.fixed.rates
+;;                             fn from R in fertility module, to use
+;;                             until a Clojure version exists
+
+(def fertility-inputs (ld/load-datasets
+                       {:historic-asfr
+                        "resources/test_data/fertility/bristol_historic_asfr.csv"
+                        :births-age-sex-mother
+                        "resources/test_data/fertility/bristol_proj_births_age_sex_mother_2015.csv"}))
+
+(def params {:fert-last-yr 2014 :pm (/ 105 205)})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; R outputs for comparison ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def fertility-outputs-r (ld/load-datasets
+                          {:projected-asfr-finalyrfixed
+                           "resources/test_data/fertility/bristol_initial_projected_fertility_rates_2015.csv"
+                           :births-by-sex
+                           "resources/test_data/fertility/bristol_proj_births_by_sex_2015.csv"}))
+
+(deftest project-asfr-finalyrhist-fixed-test
+  (testing "Fertiliy rates are projected correctly."
+    (let [proj-asfr-r (-> fertility-outputs-r
+                          :projected-asfr-finalyrfixed
+                          (ds/rename-columns {:fert-rate :fert-rate-r}))
+          joined-asfr (-> fertility-inputs
+                          (project-asfr-finalyrhist-fixed params)
+                          :initial-projected-fertility-rates
+                          (wds/join proj-asfr-r [:gss-code :sex :age]))]
+      (is (every? #(fp-equals? (i/sel joined-asfr :rows % :cols :fert-rate-r)
+                               (i/sel joined-asfr :rows % :cols :fert-rate)
+                               0.0000000001)
+                  (range (first (:shape joined-asfr))))))))
+
+
+(deftest combine-into-births-by-sex-test
+  (testing "Births by sex have been gathered correctly & match R output"
+    (let [births-by-sex-r (-> fertility-outputs-r
+                              :births-by-sex
+                              (ds/rename-columns {:births :births-r}))
+          joined-births-by-sex (-> fertility-inputs
+                                   :births-age-sex-mother
+                                   (combine-into-births-by-sex (:pm params))
+                                   (wds/join births-by-sex-r [:gss-code :sex]))]
+      (is (every? #(fp-equals? (i/sel joined-births-by-sex :rows % :cols :births-r)
+                               (i/sel joined-births-by-sex :rows % :cols :births)
+                               0.0000000001)
+                  (range (first (:shape joined-births-by-sex))))))))
