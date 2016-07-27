ceded4712272c8c694183b15e09db7dbe0971ca5
Eleonore9 <eleonore.mayola@ymail.com>
2016-07-27 18:26:22 +0100

    Update dependencies (except workspace-api).

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [prismatic/schema "1.1.2"]
-                 [net.mikera/core.matrix "0.51.0"]
+                 [prismatic/schema "1.1.3"]
+                 [net.mikera/core.matrix "0.52.2"]
                  [kixi/incanter-core "1.9.1-p0-3bf644a"]
                  [org.clojure/data.csv "0.1.3"]
                  [witan.workspace-api "0.1.12"]
                  [org.clojure/tools.cli "0.3.5"]
-                 [com.taoensso/timbre "4.7.0"]]
+                 [com.taoensso/timbre "4.7.3"]]
   :target-path "target/%s"
   :profiles {:dev {:source-paths ["src" "src-cli"]
                    :dependencies [[witan.workspace-executor "0.2.2"
@@ -18,6 +18,6 @@
              :uberjar {:aot :all}
              :cli {:main witan.models.run-models
                    :source-paths ["src" "src-cli"]
-                   :dependencies [[witan.workspace-executor "0.2.1"
+                   :dependencies [[witan.workspace-executor "0.2.2"
                                    :exclusions [witan.workspace-api]]]}}
   :exclusions [prismatic/schema org.clojure/clojure])
