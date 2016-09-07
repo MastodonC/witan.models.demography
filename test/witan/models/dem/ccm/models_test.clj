cab9d159fd63d9400f7ccf35622af1dac88d0980
Antony Woods <antony@mastodonc.com>
2016-09-07 15:54:15 +0100

    Added duplicate test

--- test/witan/models/dem/ccm/models_test.clj
+++ test/witan/models/dem/ccm/models_test.clj
@@ -15,21 +15,29 @@
 (deftest validate-models
   (let [library (m/model-library)
         funs    (p/available-fns library)]
-    (doseq [{:keys [catalog metadata workflow]} (p/available-models library)]
-      (let [{:keys [witan/name witan/version]} metadata
-            model-name (str name " " version)]
-        (testing (str "Is this model valid? - " model-name)
-          (is catalog)
-          (is metadata)
-          (doseq [{:keys [witan/name witan/fn witan/version witan/params]} catalog]
-            (testing (str "\n> testing catalog entry " name " " version)
-              (let [fnc (some #(when (and (= fn (:witan/name %))
-                                          (= version (:witan/version %))) %) funs)]
-                (is fnc) ;; if fail, can't find function with this name + version
-                ;; only check 'function' types for params
-                (when (and fnc (= (:witan/type fnc) :function))
-                  (let [{:keys [witan/param-schema]} fnc]
-                    (when (or params param-schema)
-                      (is params)
-                      (is param-schema)
-                      (is (not (s/check param-schema params))))))))))))))
+    (testing "Are the model's valid?"
+      (doseq [{:keys [catalog metadata workflow]} (p/available-models library)]
+        (let [{:keys [witan/name witan/version]} metadata
+              model-name (str name " " version)]
+          (testing (str "\n> Model: " model-name)
+            (is catalog)
+            (is metadata)
+            (doseq [{:keys [witan/name witan/fn witan/version witan/params]} catalog]
+              (testing (str "\n>> testing catalog entry " name " " version)
+                (let [fnc (some #(when (and (= fn (:witan/name %))
+                                            (= version (:witan/version %))) %) funs)]
+                  (is fnc) ;; if fail, can't find function with this name + version
+                  ;; only check 'function' types for params
+                  (when (and fnc (= (:witan/type fnc) :function))
+                    (let [{:keys [witan/param-schema]} fnc]
+                      (when (or params param-schema)
+                        (is params)
+                        (is param-schema)
+                        (is (not (s/check param-schema params)))))))))))))
+    (testing "Are there duplicates in contracts?"
+      (let [counts-by-key (reduce (fn [a [k v]]
+                                    (assoc a k (count v))) {}
+                                  (group-by (juxt :witan/name :witan/version) funs))]
+        (doseq [[[name version] num] counts-by-key]
+          (testing (str "\n> testing contract function " name " " version)
+            (is (= 1 num))))))))
