53d574874efdfd99d6c716903a3003902d3990bb
Seb Bailey <sebastian@mastodonc.com>
2017-02-06 17:44:53 +0000

    Output to csv

--- src-repl/run_model.clj
+++ src-repl/run_model.clj
@@ -5,6 +5,7 @@
             [witan.workspace-api.protocols :as p]
             [schema.core :as s]
             [witan.workspace-executor.core :as wex]
+            [clojure.core.matrix.dataset :as ds]
             [witan.models.dem.ccm.models :refer [cohort-component-model
                                                  model-library]]))
 
@@ -27,15 +28,24 @@
   [local-inputs input]
   (assoc-in input [:witan/params :fn] (partial local-download local-inputs input)))
 
+(defn convert-row-maps-to-vector-of-vectors
+  "Takes a sequence of maps and returns a vector of vectors where the first vector
+   contains column names and the following vetors contain the value for each
+   row. This is the format needed to save as csv using the write-to-csv function"
+  [rows-as-maps]
+  (let [colnames (mapv name (keys (first rows-as-maps)))
+        rows (mapv #(vec (vals %)) rows-as-maps)]
+    (into [colnames] rows)))
+
+(defn to-csv [data filepath]
+  (with-open [out-file (io/writer filepath)]
+    (data-csv/write-csv out-file data)))
+
 (defn run-demography-model
   ""
   [gss-code
-   first-proj-year last-proj-year
-   start-year-avg-domin-mig end-year-avg-domin-mig start-year-avg-domout-mig end-year-avg-domout-mig
-   start-year-avg-intin-mig end-year-avg-intin-mig start-year-avg-intout-mig end-year-avg-intout-mig
-   proj-asfr-variant future-fert-scenario
-   start-year-avg-mort end-year-avg-mort proj-asmr-variant  mort-scenario
-   proportion-male-newborns]
+   in-migrant-multiplier
+   first-proj-year last-proj-year]
   (let [local-inputs {:ccm-core-input/historic-population
                       [:historic-population
                        (with-gss "./datasets/default_datasets/core/historic_population" gss-code)]
@@ -66,15 +76,24 @@
                       :ccm-core-input/international-out-migrants
                       [:international-out-migrants
                        (with-gss "./datasets/default_datasets/migration/historic_migration_flows_international_out" gss-code)]}
-        fixed-catalog (mapv #(-> (if (= (:witan/type %) :input)
-                                   (fix-input local-inputs %)
-                                   %)
-                                 (assoc-in [:witan/params :last-proj-year] last-proj-year)
-                                 (assoc-in [:witan/params :first-proj-year] first-proj-year))
-                            (:catalog cohort-component-model))
+        catalog (mapv #(-> (if (= (:witan/type %) :input)
+                             (fix-input local-inputs %)
+                             %)
+                           (assoc-in [:witan/params :first-proj-year] first-proj-year)
+                           (assoc-in [:witan/params :last-proj-year] last-proj-year)
+                           (assoc-in [:witan/params :in-migrant-multiplier] in-migrant-multiplier))
+                      (:catalog cohort-component-model))
         workspace     {:workflow  (:workflow cohort-component-model)
-                       :catalog   fixed-catalog
+                       :catalog   catalog
                        :contracts (p/available-fns (model-library))}
         workspace'    (s/with-fn-validation (wex/build! workspace))
-        result        (wex/run!! workspace' {})]
-    (first result)))
+        result        (wex/run!! workspace' {})
+        as-rows       (map ds/row-maps (vals (first result)))
+        output        (hash-map :births (convert-row-maps-to-vector-of-vectors (nth as-rows 0))
+                                :population (convert-row-maps-to-vector-of-vectors (nth as-rows 1))
+                                :net-migration (convert-row-maps-to-vector-of-vectors (nth as-rows 2))
+                                :mortality (convert-row-maps-to-vector-of-vectors (nth as-rows 3)))]
+    (to-csv (:births output) (str "./output/" (name :births) ".csv"))
+    (to-csv (:population  output) (str "./output/" (name :population) ".csv"))
+    (to-csv (:net-migration output) (str "./output/" (name :net-migration) ".csv"))
+    (to-csv (:mortality output) (str "./output/" (name :mortality) ".csv"))))
